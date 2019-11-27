07bb3f63bef976488aa69fee081c8aa3d251d57e
Rajasekhar <rajasekhartechpy@gmail.com>
2019-11-27 16:45:07 +0530

    created the templates

--- hyreazy/rmsapp/urls.py
+++ hyreazy/rmsapp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home, name = 'home' ),
+    path('',views.index, name = 'index' ),
+    path('index',views.index, name = 'index' ),
+    path('aboutus',views.aboutus, name = 'aboutus'),
+    path('contactus',views.contactus, name = 'contactus'),
+    path('login',views.login, name = 'login'),
+    path('register',views.register, name = 'register'),
 ]
--- hyreazy/rmsapp/views.py
+++ hyreazy/rmsapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home(request):
+def index(request):
     return render(request,'index.html')
+def aboutus(request):
+    return render(request,'aboutus.html')
+def contactus(request):
+    return render(request,'contactus.html')
+def login(request):
+    return render(request,'login.html')
+def register(request):
+    return render(request,'register.html')
